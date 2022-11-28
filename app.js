28a2c982fd5508bab43454b6ada3e17ee45feddc
taimoorbaig99 <muhammadtaimoorbaig64@gmail.com>
2022-11-28 04:47:50 +0400

    javascriptform

---
+++ app.js
@@ -0,0 +1,56 @@
+// function mkc(a) {
+//     console.log(a);
+// }
+
+// var clickinput = document.getElementById('vert');
+
+// function dns(a) {
+//     var t = clickinput.value
+//     console.log(clickinput.value);
+    
+// }    
+
+// var arrayinput = document.getElementById('array');
+// var errorinput = document.getElementById('Error');
+// var a =[];
+
+
+// function opt() {
+//     var f = arrayinput ;
+//     var h = errorinput.value ;
+//     if(h == "" ) {
+//         errorinput.value = "enter value"
+//     }else{
+     
+//     console.log(errorinput.value);
+//     a.push(errorinput.value);
+//     console.log(a);
+   
+//     }
+    
+// }
+
+function abc(a) {
+    console.log(a);
+};
+
+var firstnameinput = document.getElementById('firstname');
+var lastnameinput = document.getElementById('lastname');
+var ftrnameinput = document.getElementById('ftrname');
+var cnicinput = document.getElementById('cnic');
+var cntctinput = document.getElementById('cntct');
+var pswrdinput = document.getElementById('pswrd');
+function mkc() {
+    var t = firstnameinput.value ;
+    var m = lastnameinput.value ;
+    var n = ftrnameinput.value ;
+    var d = cnicinput.value ;
+    var b = cntctinput.value ;
+    var w = pswrdinput.value ;
+    console.log(firstnameinput.value);
+    console.log(lastnameinput.value);
+    console.log(ftrnameinput.value);
+    console.log(cnicinput.value);
+    console.log(cntctinput.value);
+    console.log(pswrdinput.value);
+}
